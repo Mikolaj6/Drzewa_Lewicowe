d77865497a926849bbd88c5325ca8060052b6342
Mikolaj6 <32999228+Mikolaj6@users.noreply.github.com>
2017-11-11 17:30:34 +0100

    Komentarze

--- leftist.ml
+++ leftist.ml
@@ -8,11 +8,13 @@ let singleton x = Node (Leaf,x,Leaf,1)
 
 let empty = Leaf
 
+(*mierzy najmniejsza odleglosc od korzenia do liscia najbardziej z prawej*)
 let rank = 
 	function
 		| Leaf -> 0
 		| Node(_,_,_,r) -> r
-		
+
+
 let rec join q_a q_b = 
 	match q_a,q_b with
 		| Leaf,q -> q
@@ -20,9 +22,13 @@ let rec join q_a q_b =
 		| Node(l,k_a,r,_),Node(_,k_b,_,_) ->
 			if k_a>k_b then join q_b q_a
 			else 
+				(*rekurencyjnie laczy poddrzewa*)
 				let joined = join r q_b in
+				(*bierze najkrotsza droge z lewego poddrzewa*)
 				let rank_left = rank l in
+				(*bierze najkrotsza droge z lewego poddrzewa*)
 				let rank_right = rank joined in
+				(*po sklejeniu ustawiamy poddrzewa na "dobra strone"*)
 				if rank_left>=rank_right then
 					Node(l,k_a,joined,rank_right+1)
 				else
