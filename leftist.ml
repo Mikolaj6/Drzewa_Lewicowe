ec86aafd74ca646bd5dedfffb97abd9a37aa6831
Mikolaj6 <32999228+Mikolaj6@users.noreply.github.com>
2017-11-11 18:42:40 +0100

    Final Version

--- leftist.ml
+++ leftist.ml
@@ -1,26 +1,37 @@
+(* Mikolaj Grzywacz
+Code Review: Andrzej Gluszak*)
+
+(* lewe poddrzewo * wartosc * prawe poddrzewo * prawa wyskosc *)
 type 'a queue =
 	| Leaf
-	| Node of 'a queue*'a*'a queue*int
+	| Node of 'a queue * 'a * 'a queue * int
 	
 exception Empty
 
-let singleton x = Node (Leaf,x,Leaf,1)
+(* konstruktory *)
+let singleton x = Node (Leaf, x, Leaf, 1)
 
 let empty = Leaf
 
-(*mierzy najmniejsza odleglosc od korzenia do liscia najbardziej z prawej*)
+(*selektory*)
+(*mierzy najmniejsza odleglosc od korzenia do liscia najbardziej z prawej, prawa wyskosc*)
 let rank = 
 	function
 		| Leaf -> 0
 		| Node(_,_,_,r) -> r
 
-
+let is_empty =
+	function
+		| Leaf -> true
+		| Node (_,_,_,_) -> false
+		
+(*modyfikatory*)
 let rec join q_a q_b = 
-	match q_a,q_b with
-		| Leaf,q -> q
-		| q,Leaf -> q
-		| Node(l,k_a,r,_),Node(_,k_b,_,_) ->
-			if k_a>k_b then join q_b q_a
+	match q_a, q_b with
+		| Leaf, q -> q
+		| q, Leaf -> q
+		| Node (l, k_a, r, _), Node (_, k_b, _, _) -> 
+			if k_a > k_b then join q_b q_a
 			else 
 				(*rekurencyjnie laczy poddrzewa*)
 				let joined = join r q_b in
@@ -29,20 +40,17 @@ let rec join q_a q_b =
 				(*bierze najkrotsza droge z lewego poddrzewa*)
 				let rank_right = rank joined in
 				(*po sklejeniu ustawiamy poddrzewa na "dobra strone"*)
-				if rank_left>=rank_right then
-					Node(l,k_a,joined,rank_right+1)
+				if rank_left >= rank_right then
+					Node (l, k_a, joined, rank_right + 1)
 				else
-					Node(joined,k_a,l,rank_left+1)
+					Node (joined, k_a, l, rank_left + 1)
 
+(*dodaje singleton x do aktualnego drzewa*)
 let add x q =
 	join (singleton x) q
-	
-let is_empty =
-	function
-		| Leaf -> true
-		| Node(_,_,_,_) -> false
 		
+(*usuwa min_element (zwracajac go) i joinuje dwa poddrzewa*)
 let delete_min = 
 	function
 		| Leaf -> raise Empty
-		| Node(l,k,r,_) -> (k, (join l r))
+		| Node (l,k,r,_) -> (k, (join l r))
